3a2cb3458bbcf1060576efa17192bf8f70809ea0
Trent Rand <contact@trentrand.com>
2021-02-08 22:43:47 -0500

    Position visualizer's camera and add color background to 3D grids
    These changes fix-up the visualizer to look a bit better. The camera now
    points to the center of the 3D grid, which better positions the grid
    into the center of the view. Thin rectangles have been added behind the
    grid on each axis to provide background color, which isn't supported in
    the default grid drawing command.

--- visualizer/src/main.c
+++ visualizer/src/main.c
@@ -1,6 +1,8 @@
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "raylib.h"
 #include "rlgl.h"
+#include "raymath.h"
 
 int main(int argc, char *argv[argc+1]) {
   const int screenWidth = 1920;
@@ -10,7 +12,7 @@ int main(int argc, char *argv[argc+1]) {
 
   Camera camera = { 0 };
   camera.position = (Vector3){ 250.0f, 200.0f, 250.0f };
-  camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+  camera.target = (Vector3){ 0.0f, 75.0f, 0.0f };
   camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
   camera.fovy = 45.0f;
   camera.type = CAMERA_PERSPECTIVE;
@@ -26,35 +28,38 @@ int main(int argc, char *argv[argc+1]) {
   while (!WindowShouldClose()) {
     UpdateCamera(&camera);
 
-    if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-
-    if (IsKeyDown('1')) camera.position = (Vector3){ 250.0f, 250.0f, 250.0f };
-    if (IsKeyDown('2')) camera.position = (Vector3){ 250.0f, 350.0f, 250.0f };
-    if (IsKeyDown('3')) camera.position = (Vector3){ 250.0f, 450.0f, 250.0f };
-
     BeginDrawing();
     {
       ClearBackground((Color){ 242, 242, 242, 0 });
 
       BeginMode3D(camera);
       {
-        DrawCylinder((Vector3){ 0.0f, 50.0f, 0.0f }, 5.0f, 5.0f, 50.f, 100, BLACK);
-
+        DrawCubeV((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 200.0f, -0.1f, 200.0f}, (Color){ 255, 255, 255, 255});
         DrawGrid(20, 10.0f);
+
         rlPushMatrix();
           rlRotatef(90.0f, 0.0f, 0.0f, 1.0f);
           rlTranslatef(100.0f, 100.0f, 0.0f);
-          DrawGrid(20, 10.0f);
-          rlRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
 
-          rlRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-          rlTranslatef(100.0f, 100.0f, 0.0f);
+          DrawCubeV((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 200.0f, -0.1f, 200.0f}, (Color){ 255, 255, 255, 255});
           DrawGrid(20, 10.0f);
-          rlRotatef(90.0f, 1.0f, 0.0f, 0.0f);
         rlPopMatrix();
+
+        rlPushMatrix();
+          rlRotatef(90.0f, -1.0f, 0.0f, 0.0f);
+          rlTranslatef(0.0f, 100.0f, 100.0f);
+
+          DrawCubeV((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 200.0f, -0.1f, 200.0f}, (Color){ 255, 255, 255, 255});
+          DrawGrid(20, 10.0f);
+
+          rlRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
+        rlPopMatrix();
+
+        DrawCylinder((Vector3){ 0.0f, 50.0f, 0.0f }, 5.0f, 5.0f, 50.f, 100, BLACK);
       }
       EndMode3D();
 
+      DrawText("Rocket Visualizer", (screenWidth / 2) - 100, 10, 20, BLACK);
       DrawFPS(10, 10);
     }
     EndDrawing();
