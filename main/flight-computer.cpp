9b1aca4aa43c23a081e18cdcacef9b5ce8b7ea2b
Trent Rand <contact@trentrand.com>
2021-03-22 23:28:48 -0400

    Setup flight computer Wifi network connection
    These changes add support for Wifi network connection. The current
    use-case for this is to synchronize the system clock on boot.
    
    New menu items have been added to `menuconfig` which allow specification
    of the network SSID and password. The password is stored in `sdkconfig`,
    which is ignored from version control.

--- main/flight-computer.cpp
+++ main/flight-computer.cpp
@@ -6,9 +6,131 @@
 #include <string.h>
 #include <stdio.h>
 
-static const char* TAG = "BNO055";
+/* #include <string.h> */
+/* #include "freertos/FreeRTOS.h" */
+/* #include "freertos/task.h" */
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
-extern "C" void app_main() {
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "BNO055ESP32.h"
+
+static const char* TAG = "Flight Computer";
+
+extern "C" {
+
+// Event group defines bits with event statuses
+static EventGroupHandle_t s_wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static int s_retry_num = 0;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < CONFIG_ESP_WIFI_MAXIMUM_CONNECTION_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(TAG, "Attempting connection to Wifi AP");
+    } else {
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+    }
+    ESP_LOGI(TAG,"Failed to connect to Wifi AP");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+    ESP_LOGI(TAG, "Connected to Wifi! Device was assigned IP address:" IPSTR, IP2STR(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
+void wifi_connect(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+        ESP_EVENT_ANY_ID,
+        &event_handler,
+        NULL,
+        &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+        IP_EVENT_STA_GOT_IP,
+        &event_handler,
+        NULL,
+        &instance_got_ip));
+
+  wifi_config_t wifi_config = {
+    .sta = {
+      { .ssid = CONFIG_ESP_WIFI_SSID },
+      { .password = CONFIG_ESP_WIFI_PASSWORD },
+      .pmf_cfg = {
+        .capable = true,
+        .required = false
+      }
+
+    },
+  };
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+  ESP_ERROR_CHECK(esp_wifi_start() );
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+   * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+      WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+      pdFALSE,
+      pdFALSE,
+      portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+   * happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(TAG, "Successfully connected to Wifi AP with SSID:%s",
+        CONFIG_ESP_WIFI_SSID);
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s",
+        CONFIG_ESP_WIFI_SSID);
+  } else {
+    ESP_LOGE(TAG, "UNEXPECTED EVENT");
+  }
+
+  // The event will not be processed after unregister 
+  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+  ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+  vEventGroupDelete(s_wifi_event_group);
+}
+
+void app_main() {
+  // Initialize non-volatile flash storage
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  // Perform network connection
+  ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+  wifi_connect();
+
   BNO055 bno(UART_NUM_1, (gpio_num_t)CONFIG_BNO055_SCL_GPIO, (gpio_num_t)CONFIG_BNO055_SDA_GPIO);
 
   // Initialize Telemetry proto buffer
