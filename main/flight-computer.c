925ea6e3f62e4886e2c5ee000dd6dca3bf9cd9a7
Trent Rand <contact@trentrand.com>
2021-04-20 23:12:11 -0400

    Remove ShellAddicted/BNO055ESP32 from flight computer dependencies
    While it was possible to write a C language binding for this C++
    library, it didn't seem worth the trouble right now. This BNO055 driver
    will be replaced by a C library.

--- main/flight-computer.c
+++ main/flight-computer.c
@@ -8,7 +8,6 @@
 #include <driver/gpio.h>
 #include <driver/uart.h>
 
-/* #include "BNO055ESP32.h" */
 #include "./models/telemetry.pb-c.h"
 
 #include "./time.c"
@@ -54,22 +53,6 @@ void app_main() {
   uart_set_pin(uart_num, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
   ESP_ERROR_CHECK(uart_driver_install(uart_num, 128 * 2, 0, 0, NULL, 0));
 
-  // TODO: which UART do we dedicate to BNO055? Note: UART_NUM_0 is for usb telemetry stream
-  /* BNO055 bno(UART_NUM_0, (gpio_num_t)CONFIG_BNO055_SCL_GPIO, (gpio_num_t)CONFIG_BNO055_SDA_GPIO); */
-
-  /* try { */
-  /*   bno.begin(); */
-  /*   bno.enableExternalCrystal(); */
-  /*   bno.setOprModeNdof(); */
-  /*   ESP_LOGI(TAG, "Setup Done."); */
-  /* } catch (BNO055BaseException& ex) { */
-  /*   ESP_LOGE(TAG, "Setup Failed, Error: %s", ex.what()); */
-  /*   return; */
-  /* } catch (std::exception& ex) { */
-  /*   ESP_LOGE(TAG, "Setup Failed, Error: %s", ex.what()); */
-  /*   return; */
-  /* } */
-
   // Initialize Telemetry proto buffer
   Telemetry telemetry = TELEMETRY__INIT;
 
@@ -86,37 +69,6 @@ void app_main() {
     serialOutputBuffer = (uint8_t*) heap_caps_calloc(1, bufferLength, MALLOC_CAP_8BIT);
 
     telemetry__pack(&telemetry, buffer);
-
-    /* try { */
-    /* int8_t temperature = bno.getTemp(); */
-    /* ESP_LOGI(TAG, "TEMP: %d°C", temperature); */
-
-    /* int16_t sw = bno.getSWRevision(); */
-    /* uint8_t bl_rev = bno.getBootloaderRevision(); */
-    /* ESP_LOGI(TAG, "SW rev: %d, bootloader rev: %u", sw, bl_rev); */
-
-    /* bno055_self_test_result_t res = bno.getSelfTestResult(); */
-    /* ESP_LOGI(TAG, "Self-Test Results: MCU: %u, GYR:%u, MAG:%u, ACC: %u", res.mcuState, res.gyrState, res.magState, */
-    /*     res.accState); */
-    /* } catch (BNO055BaseException& ex) { */
-    /* ESP_LOGE(TAG, "Something bad happened: %s", ex.what()); */
-    /* return; */
-    /* } catch (std::exception& ex) { */
-    /* ESP_LOGE(TAG, "Something bad happened: %s", ex.what()); */
-    /* return; */
-    /* } */
-
-    /* try { */
-    /*   bno055_calibration_t cal = bno.getCalibration(); */
-    /*   bno055_vector_t v = bno.getVectorEuler(); */
-    /*   ESP_LOGI(TAG, "Euler: X: %.1f Y: %.1f Z: %.1f || Calibration SYS: %u GYRO: %u ACC:%u MAG:%u", v.x, v.y, v.z, cal.sys, */
-    /*       cal.gyro, cal.accel, cal.mag); */
-    /* } catch (BNO055BaseException& ex) { */
-    /*   ESP_LOGE(TAG, "Something bad happened: %s", ex.what()); */
-    /*   return; */
-    /* } catch (std::exception& ex) { */
-    /*   ESP_LOGE(TAG, "Something bad happened: %s", ex.what()); */
-    /* } */
 
     const size_t serialOutputBufferLength = pack_payload_to_frame(buffer, bufferLength, serialOutputBuffer);
     uart_write_bytes(uart_num, (const char*)serialOutputBuffer, serialOutputBufferLength);
